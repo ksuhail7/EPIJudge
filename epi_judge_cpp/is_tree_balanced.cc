5e634fb6881ad154dd058b50768773d5ed150c2c
Suhail Kandanur <ksuhail7@live.com>
2022-10-02 16:23:17 -0700

    solution to 10.1: is tree balanced

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,25 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
+int IsBalancedHelper(const unique_ptr<BinaryTreeNode<int>>& root)
+{
+	if (root == nullptr) return true;
+
+	int h_left = IsBalancedHelper(root->left);
+	int h_right = IsBalancedHelper(root->right);
+	if((h_left != -1 && h_right != -1) && abs(h_left - h_right) <= 1)
+	{
+		return 1 + std::max(h_left, h_right);
+	} else
+	{
+		return -1;
+	}
+
+
+}
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return true;
+  return IsBalancedHelper(tree) != -1;
 }
 
 int main(int argc, char* argv[]) {
